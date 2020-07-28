fe7e050b9ad128f4fa74801e95282b146fa20b91
renjiyun <wozaijixu@msn.cn>
2020-07-28 23:24:41 +0800

    初始化

---
+++ src/main/java/com/eggip/psyduck/PsyduckApplication.java
@@ -0,0 +1,13 @@
+package com.eggip.psyduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsyduckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eggip/psyduck/PsyduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggip.psyduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsyduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
