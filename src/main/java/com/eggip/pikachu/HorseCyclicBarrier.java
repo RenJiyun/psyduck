57fe9b690b1ff61a1aee0655d94365d5be53b0be
renjiyun <wozaijixu@msn.cn>
2020-08-12 01:26:15 +0800

    探索谁执行了马儿跑了一圈这段代码

--- src/main/java/com/eggip/pikachu/HorseCyclicBarrier.java
+++ src/main/java/com/eggip/pikachu/HorseCyclicBarrier.java
@@ -1,9 +1,8 @@
 package com.eggip.pikachu;
 
-import com.eggip.lock.BarrierDemo;
-
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
@@ -13,39 +12,41 @@ import java.util.concurrent.Executors;
  * @date 2020-08-11 22:57
  */
 public class HorseCyclicBarrier {
-    private static CyclicBarrier cyclicBarrier =new CyclicBarrier(3, new Runnable() {
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(3, new Runnable() {
         @Override
         public void run() {
+            System.out.println(Thread.currentThread().getId() + "============");
             System.out.println("马儿已经跑了一圈了");
         }
     });
-    public static class HorseRace implements  Runnable{
-      private volatile int id;
-      public HorseRace(int id){
-          this.id=id;
-      }
-        @Override
-        public void run() {
-            while(true) {
 
-            try {
-                Thread.sleep(2000);  // 跑的时间
-                System.out.println("马儿"+id+"号跑到终点了");
-                cyclicBarrier.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (BrokenBarrierException e) {
-                e.printStackTrace();
-            }
+    public static class HorseRace implements Runnable {
+        private volatile int id;
+
+        public HorseRace(int id) {
+            this.id = id;
         }
 
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(2000); // 跑的时间
+                    System.out.println("马儿" + id + "号跑到终点了");
+                    System.out.println(Thread.currentThread().getId());
+                    cyclicBarrier.await();
+                } catch (Throwable t) {}
+            }
 
         }
     }
-    public  static void main(String[] args) throws BrokenBarrierException, InterruptedException {
-              for ( int i=0;i<3;i++){
-               HorseRace horseRace=new HorseRace(i);
-               Executors.newFixedThreadPool(1).execute(horseRace);
-              }
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+        Executor executor = Executors.newFixedThreadPool(3);
+        System.out.println(Thread.currentThread().getId() + "主线程");
+        for (int i = 0; i < 3; i++) {
+            HorseRace horseRace = new HorseRace(i);
+            executor.execute(horseRace);
+        }
     }
 }
