190de77bc9cd7fc39886f0e9af769c24acc19414
renjiyun <wozaijixu@msn.cn>
2020-08-14 00:05:47 +0800

    轮询future获取值，有bug

--- src/main/java/com/eggip/concurrent/CompletionServiceDemo.java
+++ src/main/java/com/eggip/concurrent/CompletionServiceDemo.java
@@ -1,5 +1,7 @@
 package com.eggip.concurrent;
 
+import org.springframework.util.CollectionUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -16,25 +18,31 @@ public class CompletionServiceDemo {
         List<Future<Integer>> futureList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             futureList.add(executorService.submit(new Callable<Integer>() {
-
                 @Override
                 public Integer call() throws Exception {
                     // 很久才返回
-                    Thread.sleep(3000);
+                    Thread.sleep(200);
                     return 0;
                 }
 
             }));
         }
+        futureList2(futureList,n);
 
-
+    }
+    private static void futureList2(List<Future<Integer>> futureList,int n){
+        List<Future<Integer>> futureList1=new ArrayList<>();
         for (int i = 0; i < n; i++) {
             try {
                 System.out.println(futureList.get(i).get(0, TimeUnit.MICROSECONDS));
             } catch (Throwable t) {
-                t.printStackTrace();
+                futureList1.add(futureList.get(i));
                 continue;
             }
         }
+        if(!CollectionUtils.isEmpty(futureList1)){
+            futureList2(futureList1,futureList1.size());
+        }
+
     }
 }
