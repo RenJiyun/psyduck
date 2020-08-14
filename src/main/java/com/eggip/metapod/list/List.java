98e84608b94eda530e5dbfdcb785f13f3676deb0
renjiyun <wozaijixu@msn.cn>
2020-08-15 00:29:16 +0800

    自定义列表的打印

--- src/main/java/com/eggip/metapod/list/List.java
+++ src/main/java/com/eggip/metapod/list/List.java
@@ -11,4 +11,33 @@ public class List<E> {
     }
     
     public static final List<?> nil = new List<>(null, null);
+
+
+    @Override 
+    public String toString() {
+        List<E> rest = this;
+        String s = "[";
+        while (rest != nil) {
+            s += rest.head;
+            s += ", ";
+            rest = rest.tail;
+        }
+
+        if (s.length() > 1) {
+            s = s.substring(0, s.length() - 2);
+        }
+        s += "]";
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        // [1, 2, 3]
+        List<Integer> list = new List(1, new List(2, new List(3, List.nil)));
+
+
+        System.out.println(list);
+    }
+
+
 }
