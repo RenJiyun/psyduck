3d08eb722bb9ad9c41090704aa40f6ce3ac73432
renjiyun <wozaijixu@msn.cn>
2020-08-19 23:21:37 +0800

    解析choose的方法

--- src/main/java/com/eggip/dragonair/Parsers.java
+++ src/main/java/com/eggip/dragonair/Parsers.java
@@ -15,7 +15,19 @@ public class Parsers {
 
 
     public static Parser choose(Parser parser1, Parser parser2) {
-        return null;
+        return new Parser() {
+            @Override
+            public ParseResult parse(String s) {
+                ParseResult result1 = parser1.parse(s);
+                if (result1.getMatched() != "") {
+                    return result1;
+                } else {
+                    ParseResult result2 = parser2.parse(result1.getRest());
+                    return result2;
+                }
+
+            }
+        };
     }
 
 
@@ -23,6 +35,6 @@ public class Parsers {
         Parser parser_a = new CharParser('a');
         Parser parser_b = new CharParser('b');
         Parser parser_ab = choose(parser_a, parser_b);
-        System.out.println(parser_ab.parse("carfds"));
+        System.out.println(parser_ab.parse("barfds"));
     }
 }
