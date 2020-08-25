959e279e51672b3c402e48b0f085492ec44203b5
renjiyun <wozaijixu@msn.cn>
2020-08-25 17:41:21 +0800

    并发迷宫

--- src/main/java/puzzle/ConcurrentSolvePuzzle.java
+++ src/main/java/puzzle/ConcurrentSolvePuzzle.java
@@ -2,16 +2,17 @@ package puzzle;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class ConcurrentSolvePuzzle {
     private ExecutorService executorService = Executors.newFixedThreadPool(4);
     private Puzzle puzzle;
     private Position position;
     private List<Position> seedList = new ArrayList<>();
+    public ConcurrentSolvePuzzle(Puzzle puzzle,Position position){
+        this.puzzle=puzzle;
+        this.position=position;
+    }
 
     public Future<List<Position>> newTask() {
         SolvePuzzle solvePuzzle = new SolvePuzzle();
@@ -28,11 +29,43 @@ public class ConcurrentSolvePuzzle {
         }
     }
 
-    public static void main(String[] args) {
-        ConcurrentSolvePuzzle concurrentSolvePuzzle = new ConcurrentSolvePuzzle();
-        List<Future<List<Position>>> futures = new ArrayList<>();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Puzzle puzzle =new Puzzle() {
+            Position p=new Position(3);
+            Position p1=new Position(4);
+            Position p2=new Position(1);
+            @Override
+            public Position start() {
+                return new Position(4);
+            }
+
+            @Override
+            public List<Position> moveList(Position currentPosition) {
+                List<Position> positions = new ArrayList<>();
+                positions.add(p);
+                positions.add(p1);
+                positions.add(p2);
+                return positions;
+            }
+
+            @Override
+            public boolean end(Position currentPosition) {
+                Position position = new Position(1);
+                if (position.pos == currentPosition.pos) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        ConcurrentSolvePuzzle concurrentSolvePuzzle = new ConcurrentSolvePuzzle(puzzle, puzzle.start());
         for (int i = 0; i <= 5; i++) {
-            futures.add(concurrentSolvePuzzle.newTask());
+            Future<List<Position>> future=concurrentSolvePuzzle.newTask();
+            if (future.get()!=null){
+                System.out.println(future.get());
+                break;
+            }
         }
+
     }
 }
