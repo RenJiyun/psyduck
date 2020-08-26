fe303408cb2d8ca2bfcd7e2e810bac5522abb827
renjiyun <wozaijixu@msn.cn>
2020-08-26 22:22:36 +0800

    八皇后问题

--- src/main/java/puzzle/EightQueen.java
+++ src/main/java/puzzle/EightQueen.java
@@ -1,8 +1,24 @@
 package puzzle;
 
-public class EightQueen {
+import java.util.List;
+
+public class EightQueen implements Puzzle {
     private int[][] lattice=new int[8][8];
 
 
+    @Override
+    public Position start() {
 
+
+    }
+
+    @Override
+    public List<Position> moveList(Position currentPosition) {
+        return null;
+    }
+
+    @Override
+    public boolean end(Position currentPosition) {
+        return false;
+    }
 }
