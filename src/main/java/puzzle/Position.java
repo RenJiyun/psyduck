308ea74e5b9be84ce9d3e69f7555ec0448f8dc51
renjiyun <wozaijixu@msn.cn>
2020-08-29 01:16:45 +0800

    数独

--- src/main/java/puzzle/Position.java
+++ src/main/java/puzzle/Position.java
@@ -5,7 +5,6 @@ import io.vavr.Tuple3;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Stack;
 
 public class Position {
@@ -22,47 +21,48 @@ public class Position {
     public List<Tuple3<Integer, Integer, Integer>> getNumberPosition() {
         return numberPosition;
     }
-//    public Stack<Tuple2<Integer, Integer>> state = new Stack<>();
-//
-//    public Position() {
-//    }
-//
-//    public Position(Stack<Tuple2<Integer, Integer>> state) {
-//        this.state = state;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Position{" +
-//                "state=" + state +
-//                '}';
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int temp = 0;
-//        for (int i = 0; i < state.size(); i++) {
-//            temp = temp + state.get(i)._1 + state.get(i)._2;
-//        }
-//        return temp % 31;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Position position = (Position) o;
-//
-//        if (position.state.size() != this.state.size()) return false;
-//        if (position.state.size() == 0) return true;
-//
-//        for (int i = 0; i < state.size(); i++) {
-//            if (!position.state.get(i).equals(this.state.get(i))) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
+
+    public Stack<Tuple2<Integer, Integer>> state = new Stack<>();
+
+    public Position() {
+    }
+
+    public Position(Stack<Tuple2<Integer, Integer>> state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "state=" + state +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int temp = 0;
+        for (int i = 0; i < state.size(); i++) {
+            temp = temp + state.get(i)._1 + state.get(i)._2;
+        }
+        return temp % 31;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+
+        if (position.state.size() != this.state.size()) return false;
+        if (position.state.size() == 0) return true;
+
+        for (int i = 0; i < state.size(); i++) {
+            if (!position.state.get(i).equals(this.state.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 }
