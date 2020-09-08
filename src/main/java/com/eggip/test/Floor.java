3d39f98fb19a7c595c8850d62e704ff12a81ca1e
renjiyun <wozaijixu@msn.cn>
2020-09-09 00:06:18 +0800

    楼层

---
+++ src/main/java/com/eggip/test/Floor.java
@@ -0,0 +1,47 @@
+package com.eggip.test;
+
+
+import java.util.List;
+
+public class Floor {
+
+    int floorNumber = 1;//从零层开始
+    int costNumber = 0;//从第0阶开始
+
+    public List minFloor1(int floor, int cost, List list) {
+        if (floorNumber == floor) {
+            list.add(costNumber);
+            return list;
+        }
+        if (floorNumber != floor && costNumber > cost) {
+            return null;//超过了cost，没有到达顶层
+        }
+        costNumber++;//一阶一阶的消耗
+        list.add(costNumber);
+        if (costNumber > cost) {
+            return minFloor1(++floorNumber, cost, list);
+        } else {
+            return minFloor1(floorNumber, cost, list);
+        }
+    }
+
+//    public List minFloor2(int n, List list) {
+//        if (i == n) {
+//            list.add(i);
+//            return list;
+//        }
+//        return   minFloor1(i+2,list);
+//
+//    }
+//     int k=1;
+//    public List minFloor3(int n, List list) {
+//        return null;
+//
+//    }
+//
+//    public List minFloor14(int n, List list) {
+//
+//        return null;
+//    }
+
+}
